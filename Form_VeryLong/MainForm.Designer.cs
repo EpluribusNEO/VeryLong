9f447c4e87cb8939260f5b803184c1971924e7ce
Hikaru <icestrelok@gmail.com>
2018-04-29 22:15:42 +0400

    Добавлена кномка "Copy" - события пока не обработаны

--- Form_VeryLong/MainForm.Designer.cs
+++ Form_VeryLong/MainForm.Designer.cs
@@ -36,6 +36,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox_FirstValue
@@ -83,7 +84,7 @@
             this.textBox_Result.Name = "textBox_Result";
             this.textBox_Result.ReadOnly = true;
             this.textBox_Result.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
-            this.textBox_Result.Size = new System.Drawing.Size(623, 23);
+            this.textBox_Result.Size = new System.Drawing.Size(551, 23);
             this.textBox_Result.TabIndex = 5;
             // 
             // label1
@@ -116,11 +117,22 @@
             this.label3.TabIndex = 8;
             this.label3.Text = "Result:";
             // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(666, 96);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(60, 30);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Copy";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(738, 141);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -148,6 +160,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
     }
 }
 
